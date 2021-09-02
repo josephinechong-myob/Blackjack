d66ad2281b7711bdeda0e0bc848055aa45e42f20
Josephine Chong <josephine.chong@myob.com>
2021-09-02 11:56:33 +1000

    Adding inline threory test to Hand tests

--- BlackjackTest/HandTest.cs
+++ BlackjackTest/HandTest.cs
@@ -9,6 +9,30 @@ namespace BlackjackTest
     public class HandTest
     {
         //convert this test to theory**
+        [Theory]
+        [InlineData(Rank.Eight, Rank.Ace, Rank.Two, 21)]
+        [InlineData(Rank.Ten, Rank.King, Rank.Ace, 21)]
+        [InlineData(Rank.Queen, Rank.King, Rank.Five, 25)]
+        //[InlineData(Rank.Ace, Rank.King, Rank.Ace, 12)]
+        [InlineData(Rank.Nine, Rank.Nine, Rank.Four, 22)]
+        public void Theory_HandShouldIncreaseAfterAddingACard(Rank firstRank, Rank secondRank, Rank thirdRank, int total)
+        {
+            //Arrange
+            var firstCard = new Card(firstRank, Suit.Club);
+            var secondCard = new Card(secondRank, Suit.Diamond);
+            var thirdCard = new Card(thirdRank, Suit.Club);
+            var hand = new Hand(firstCard, secondCard);
+            var expectedCountOfHandCards = 3;
+            
+            //Act
+            hand.AddCardToHand(thirdCard);
+            var actualTotal = HandEvaluator.GetTotal(hand);
+
+            //Assert
+            Assert.Equal(total, actualTotal);
+            Assert.True(hand.Cards.Contains(thirdCard));
+            Assert.Equal(expectedCountOfHandCards, hand.Cards.Count);
+        }
         
         //Adding a card to the Hand
         [Fact]
