c2287bc55a321e5665a39806c278714dd31ff4e6
Josephine Chong <josephine.chong@myob.com>
2021-09-02 14:01:20 +1000

    Added more Inline theory testing for Hand tests

--- BlackjackTest/HandTest.cs
+++ BlackjackTest/HandTest.cs
@@ -26,6 +26,7 @@ namespace BlackjackTest
             
             //Act
             hand.AddCardToHand(thirdCard);
+            hand.SortHand();
             var actualTotal = HandEvaluator.GetTotal(hand);
 
             //Assert
@@ -34,6 +35,101 @@ namespace BlackjackTest
             Assert.Equal(expectedCountOfHandCards, hand.Cards.Count);
         }
         
+        [Theory]
+        [InlineData(Rank.Eight, Rank.Ace, Rank.Two, 21)]
+        [InlineData(Rank.Ten, Rank.Ace, Rank.King, 21)]
+        [InlineData(Rank.Queen, Rank.Five, Rank.King, 25)]
+        [InlineData(Rank.Ace, Rank.Ace, Rank.King, 12)]
+        [InlineData(Rank.Nine, Rank.Four, Rank.Nine, 22)]
+        public void Theory_HandShouldSortCards(Rank firstRank, Rank secondRank, Rank thirdRank, int total)
+        {
+            //arrange
+            var firstCard = new Card(firstRank, Suit.Club);
+            var secondCard = new Card(secondRank, Suit.Diamond);
+            var thirdCard = new Card(thirdRank, Suit.Club);
+            var hand = new Hand(firstCard, secondCard);
+            
+            //act
+            hand.AddCardToHand(thirdCard);
+            hand.SortHand();
+
+            //assert
+            Assert.Equal(secondCard, hand.Cards.Last());
+        }
+        
+        [Theory]
+        [InlineData(Rank.Eight, Rank.Ace, 19)]
+        [InlineData(Rank.Ten, Rank.Ace, 21)]
+        [InlineData(Rank.Queen, Rank.Five, 15)]
+        [InlineData(Rank.Ace, Rank.Ace, 12)]
+        [InlineData(Rank.Nine, Rank.Four, 13)]
+        public void Theory_TotalOfTwoCardHandShouldBeSumOfRank(Rank firstRank, Rank secondRank, int total)
+        {
+            //arrange
+            var firstCard = new Card(firstRank, Suit.Club);
+            var secondCard = new Card(secondRank, Suit.Diamond);
+            var hand = new Hand(firstCard, secondCard);
+            
+            //act
+            hand.SortHand();
+            var result = HandEvaluator.GetTotal(hand);
+
+            //assert
+            Assert.Equal(total, result);
+        }
+        
+        [Theory]
+        [InlineData(Rank.Eight, Rank.King, 18)]
+        [InlineData(Rank.Ten, Rank.Jack, 20)]
+        [InlineData(Rank.Queen, Rank.Five, 15)]
+        [InlineData(Rank.King, Rank.King, 20)]
+        [InlineData(Rank.Queen, Rank.Queen, 20)]
+        [InlineData(Rank.Jack, Rank.Jack, 20)]
+        [InlineData(Rank.Queen, Rank.Jack, 20)]
+        [InlineData(Rank.Queen, Rank.King, 20)]
+        [InlineData(Rank.King, Rank.Jack, 20)]
+        [InlineData(Rank.Queen, Rank.Ace, 21)]
+        public void Theory_FaceCardsShouldValueTenForScoring(Rank firstRank, Rank secondRank, int total)
+        {
+            //arrange
+            var firstCard = new Card(firstRank, Suit.Club);
+            var secondCard = new Card(secondRank, Suit.Diamond);
+            var hand = new Hand(firstCard, secondCard);
+            
+            //act
+            hand.SortHand();
+            var result = HandEvaluator.GetTotal(hand);
+
+            //assert
+            Assert.Equal(total, result);
+        }
+        
+        [Theory]
+        [InlineData(Rank.Ace, Rank.King, 21)]
+        [InlineData(Rank.Ten, Rank.Ace, 21)]
+        [InlineData(Rank.Ace, Rank.Ace, 12)]
+        [InlineData(Rank.Two, Rank.Ace, 13)]
+        [InlineData(Rank.Queen, Rank.Ace, 21)]
+        [InlineData(Rank.Jack, Rank.Ace, 21)]
+        [InlineData(Rank.King, Rank.Ace, 21)]
+        [InlineData(Rank.Ace, Rank.Nine, 20)]
+        [InlineData(Rank.Eight, Rank.Ace, 19)]
+        [InlineData(Rank.Three, Rank.Ace, 14)]
+        public void Theory_AcesShouldValueEitherOneOrElevenDependingOnSumOfNonAceCards(Rank firstRank, Rank secondRank, int total)
+        {
+            //arrange
+            var firstCard = new Card(firstRank, Suit.Club);
+            var secondCard = new Card(secondRank, Suit.Diamond);
+            var hand = new Hand(firstCard, secondCard);
+            
+            //act
+            hand.SortHand();
+            var result = HandEvaluator.GetTotal(hand);
+
+            //assert
+            Assert.Equal(total, result);
+        }
+        /*
         //Adding a card to the Hand
         [Fact]
         public void HandShouldIncreaseAfterAddingACard()
@@ -68,22 +164,6 @@ namespace BlackjackTest
 
             //assert
             Assert.Equal(expectedThridCard, hand.Cards.Last());
-        }
-
-        [Fact]
-        public void NewHandShouldHaveTwoCards()
-        {
-            //arrange
-            var expectedCountOfHandCards = 2;
-            var firstCard = new Card(Rank.Ace, Suit.Club);
-            var secondCard = new Card(Rank.Eight, Suit.Diamond);
-            var hand = new Hand(firstCard, secondCard);
-            
-            //act
-            var actualCountOfHandCards = hand.Cards.Count;
-            
-            //assert
-            Assert.Equal(expectedCountOfHandCards, actualCountOfHandCards);
         }
         
         [Fact]
@@ -144,6 +224,22 @@ namespace BlackjackTest
             
             //assert
             Assert.Equal(18, result);
+        }
+*/
+        [Fact]
+        public void NewHandShouldHaveTwoCards()
+        {
+            //arrange
+            var expectedCountOfHandCards = 2;
+            var firstCard = new Card(Rank.Ace, Suit.Club);
+            var secondCard = new Card(Rank.Eight, Suit.Diamond);
+            var hand = new Hand(firstCard, secondCard);
+            
+            //act
+            var actualCountOfHandCards = hand.Cards.Count;
+            
+            //assert
+            Assert.Equal(expectedCountOfHandCards, actualCountOfHandCards);
         }
         
         [Fact]
