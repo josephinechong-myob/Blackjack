f41aa4c2a584cfde48b913e725fd4318f75ef94a
Josephine Chong <josephine.chong@myob.com>
2021-09-16 11:01:57 +1000

    adding scoring function keep track of wins

--- BlackjackGame/BlackjackGame.cs
+++ BlackjackGame/BlackjackGame.cs
@@ -10,6 +10,8 @@ namespace Blackjack
         private readonly List<IBlackjackParticipant> _participants;
         private readonly IConsole _gameConsole;
         private readonly IDeck _deck;
+        private List<string> _RecordOfWins;
+            //scoring for several games
 
         public BlackjackGame(IConsole console, IDeck deck)
         {
@@ -18,6 +20,7 @@ namespace Blackjack
             var player = new Player(_deck.DrawRandomCard(), _deck.DrawRandomCard(), _gameConsole, "Jo");
             var dealer = new Dealer(_deck.DrawRandomCard(), _deck.DrawRandomCard(), _gameConsole, "Dealer");
             _participants = new List<IBlackjackParticipant>() {player, dealer};
+            _RecordOfWins = new List<string>();
         }
 
         private List<IBlackjackParticipant> ParticipantsOrderedByScore()
@@ -26,10 +29,11 @@ namespace Blackjack
             return orderedParticipants;
         }
 
-        private void DetermineHighestScorers(List<IBlackjackParticipant> orderedParticipant, IBlackjackParticipant highestScoringPlayer)
+        private string DetermineHighestScorers(List<IBlackjackParticipant> orderedParticipant, IBlackjackParticipant highestScoringPlayer)
         {
             var highestScorer = orderedParticipant.Where(m => m.Score == highestScoringPlayer.Score).ToList();
-                
+            var winnerName = "";  
+            
             if (highestScorer.Count > 1)
             {
                 _gameConsole.WriteLine("\nIt's a tie!");
@@ -38,21 +42,49 @@ namespace Blackjack
             if (highestScorer.Count <= 1 && highestScoringPlayer.GetType() == typeof(Player))
             {
                 _gameConsole.WriteLine("\nPlayer has beat the dealer!");
+                winnerName = highestScoringPlayer.Name;
             }
 
             else if (highestScoringPlayer.GetType() == typeof(Dealer))
             {
-                _gameConsole.WriteLine("\nDealer wins!"); 
+                _gameConsole.WriteLine("\nDealer wins!");
+                winnerName = highestScoringPlayer.Name;
             }
+            return winnerName;
         }
         
-        private void FindTheWinner()
+        private string FindTheWinner()
         {
             var orderedParticipant = ParticipantsOrderedByScore();
-            var highestScoringPlayer = orderedParticipant.FirstOrDefault(); 
+            var highestScoringPlayer = orderedParticipant.FirstOrDefault();
+            var winner = "";
             if (highestScoringPlayer != null)
             {
-                DetermineHighestScorers(orderedParticipant, highestScoringPlayer);
+                winner = DetermineHighestScorers(orderedParticipant, highestScoringPlayer);
+            }
+
+            return winner;
+        }
+
+        private void KeepTrackOfWins() //dictionary - write test
+        {
+            var winnersName = FindTheWinner();
+            _RecordOfWins.Add(winnersName);
+            
+            var count = _RecordOfWins.Count(s => s != null && s.Equals(winnersName));
+            
+            var dictionary = new Dictionary<string, int>();
+            dictionary.Add(winnersName, count);
+            
+            foreach( string name in dictionary )
+            {
+                Console.WriteLine("Key = {0}", name);
+            }
+
+            //(Dealer, Jo, Dealer, Dealer) - Dealer wins 3 times, Jo wins 1 time
+            foreach(var winner in _RecordOfWins)
+            {
+                if(winner == "Dealer")
             }
         }
 
